bf74207438fa29ae84ae563bb3248d74c2c53886
roberto <robertokantovitzlara@hotmail.com>
2025-12-14 10:35:03 -0300

    criação das regras de negocio e controller da tabela receitas

--- src/main/java/com/roberto/Livro_de_Receitas/controller/ReceitasController.java
+++ src/main/java/com/roberto/Livro_de_Receitas/controller/ReceitasController.java
@@ -1,5 +1,53 @@
 package com.roberto.Livro_de_Receitas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.roberto.Livro_de_Receitas.model.ReceitasDB;
+import com.roberto.Livro_de_Receitas.service.ReceitasService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/receitas")
 public class ReceitasController {
 
+    //CRIAR O CONSTRUTOR PRA INSTANCIAR A CLASSE DO RECEITAS SERVICE
+    private final ReceitasService receitasService;
+    public ReceitasController(ReceitasService receitasService){
+        this.receitasService = receitasService;
+    } 
+
+    //CLASSE PARA LISTAR AS RECEITAS 
+    @GetMapping
+    public List<ReceitasDB> listarReceitas() {
+        return receitasService.listarReceitas();
+    }
+
+    //CLASSE PARA RETORNAR A RECEITA PELO ID
+    @GetMapping("/{id}")
+    public ResponseEntity<ReceitasDB> buscarReceita(@PathVariable Long id) {
+        return receitasService.buscarPorId(id)              //BUSCA A RECEITA PELO ID
+                .map(ResponseEntity::ok)                    //CONFIRMA SE ACHOU A RECEITA COM OK
+                .orElse(ResponseEntity.notFound().build()); //SENÃO DIZ QUE NÃO ENCONTROU
+    }
+    
+    //
+    @PostMapping
+    public String postMethodName(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+    
 }
--- src/main/java/com/roberto/Livro_de_Receitas/service/ReceitasService.java
+++ src/main/java/com/roberto/Livro_de_Receitas/service/ReceitasService.java
@@ -6,6 +6,7 @@
 package com.roberto.Livro_de_Receitas.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -14,13 +15,31 @@ import com.roberto.Livro_de_Receitas.repository.ReceitasRepository;
 
 @Service
 public class ReceitasService {
-    private final ReceitasRepository receitasRepository;
 
+    // INSTANCIAR A CLASSE DE RECEITASREPOSITORY
+    private final ReceitasRepository receitasRepository;
     public ReceitasService (ReceitasRepository receitasRepository){
         this.receitasRepository = receitasRepository;
     }
 
+    //CLASSE PARA LISTAR TODOS OS DADOS DA TABELA RECEITAS
     public List<ReceitasDB> listarReceitas(){
         return receitasRepository.findAll();
     }
+
+    //CLASSE QUE BUSCA OS DADOS POR ID
+    public Optional<ReceitasDB> buscarPorId(Long id){
+        return receitasRepository.findById(id);
+    }
+
+    //CLASSE PARA SALVAR AS RECEITAS
+    public ReceitasDB salvarReceita(ReceitasDB receita){
+        return receitasRepository.save(receita);
+    }
+
+    //CLASSE PARA DELETAR UMA RECEITA
+    public void deletarReceita(Long id){
+        receitasRepository.deleteById(id);
+    }
+
 }
